9bfa182e5f4f1c713024c7bd804e1e3ff02b4951
WI-Jeong <cpdls23@naver.com>
2023-08-18 22:04:00 +0900

    unreal engine

---
+++ Source/U_SAC/U_SACGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "U_SACGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class U_SAC_API AU_SACGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
